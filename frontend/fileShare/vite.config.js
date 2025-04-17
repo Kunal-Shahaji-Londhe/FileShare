df24bc30175c38ed32d1ee71b09c210611bd3cb9
Kunal Londhe <kunal.shahaji.londhe@gmail.com>
2025-04-17 21:59:50 +0530

    refactor: enhance Vite configuration to load environment variables for server settings

--- frontend/fileShare/vite.config.js
+++ frontend/fileShare/vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-//import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: './postcss.config.js',
-  },
-  server: {
-    port: 5173,
-    host: true,
-  },
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return defineConfig({
+    plugins: [react()],
+    css: {
+      postcss: './postcss.config.js',
+    },
+    server: {
+      port: env.VITE_SERVER_PORT || 5173,
+      host: true,
+      allowedHosts: [
+        'localhost',
+        env.VITE_SERVER_HOST
+      ]
+    },
+  })
+}
